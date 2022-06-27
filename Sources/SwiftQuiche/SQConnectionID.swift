0f00b00cdd8b6e187e3cdf8d565918e5f008c43f
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-06-27 15:32:11 -0300

    WIP: Added SQConfigTests

---
+++ Sources/SwiftQuiche/SQConnectionID.swift
@@ -0,0 +1,7 @@
+//  Copyright Kenneth Laskoski. All Rights Reserved.
+//  SPDX-License-Identifier: Apache-2.0
+
+import XCQuiche
+
+public struct SQConnectionID {
+}
--- Sources/SwiftQuiche/SwiftQuiche.swift
+++
@@ -1,6 +0,0 @@
-public struct SwiftQuiche {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
