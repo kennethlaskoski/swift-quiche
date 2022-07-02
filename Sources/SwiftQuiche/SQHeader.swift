5243f81819c603a04868266b4c76252c5ef78a37
Kenneth Laskoski <kennethlaskoski@gmail.com>
2022-07-01 22:54:47 -0300

    Added SQHeader tests

--- Sources/SwiftQuiche/SQHeader.swift
+++ Sources/SwiftQuiche/SQHeader.swift
@@ -1,11 +1,42 @@
 //  Copyright Kenneth Laskoski. All Rights Reserved.
 //  SPDX-License-Identifier: Apache-2.0
 
+import XCQuiche
+
 public struct SQHeader {
   let type: SQType
   let version: SQVersion
   let dcid: SQConnectionID
   let scid: SQConnectionID
-  let token: SQAddressToken?
+  let token: [UInt8]?
   let versions: [SQVersion]?
+}
+
+public func sqHeaderInfo(buffer: [UInt8], localConnectionIDLength: Int, tokenLength: Int) throws -> (SQType, SQVersion, SQConnectionID, SQConnectionID, [UInt8]) {
+  var type: UInt8 = 0
+  var version: UInt32 = 0
+
+  var scid = [UInt8](repeating: 0, count: SQConnectionID.maxLength)
+  var scid_len = SQConnectionID.maxLength
+
+  var dcid = [UInt8](repeating: 0, count: SQConnectionID.maxLength)
+  var dcid_len = SQConnectionID.maxLength
+
+  var token = [UInt8](repeating: 0, count: tokenLength)
+  var token_len = tokenLength
+
+  let rc = quiche_header_info(buffer, buffer.count, localConnectionIDLength, &version,
+                              &type, &scid, &scid_len, &dcid, &dcid_len,
+                              &token, &token_len)
+  guard rc >= 0 else {
+    throw SQError.invalidPacket
+  }
+
+  return (
+    SQType(rawValue: type)!,
+    SQVersion(rawValue: version),
+    SQConnectionID(bytes: scid),
+    SQConnectionID(bytes: dcid),
+    token
+  )
 }
--- Sources/SwiftQuiche/SwiftQuiche.swift
+++ Sources/SwiftQuiche/SwiftQuiche.swift
@@ -7,32 +7,3 @@ public let sqVersion = SQVersion(rawValue: UInt32(QUICHE_PROTOCOL_VERSION))
 public func isSupported(version: SQVersion) -> Bool {
   quiche_version_is_supported(version.rawValue)
 }
-
-public func sqHeaderInfo<Token: SQAddressToken>(buffer: [UInt8]) throws -> (SQType?, SQVersion, SQConnectionID, SQConnectionID, Token) {
-  var type: UInt8 = 0
-  var version: UInt32 = 0
-
-  var scid = [UInt8](repeating: 0, count: SQConnectionID.maxLength)
-  var scid_len = SQConnectionID.maxLength
-
-  var dcid = [UInt8](repeating: 0, count: SQConnectionID.maxLength)
-  var dcid_len = SQConnectionID.maxLength
-
-  var token = [UInt8](repeating: 0, count: Token.maxLength)
-  var token_len = Token.maxLength
-
-  let rc = quiche_header_info(buffer, buffer.count, SQConnectionID.maxLength, &version,
-                              &type, &scid, &scid_len, &dcid, &dcid_len,
-                              &token, &token_len);
-  if (rc < 0) {
-    throw SQError.invalidPacket
-  }
-
-  return (
-    SQType(rawValue: type),
-    SQVersion(rawValue: version),
-    SQConnectionID(bytes: scid),
-    SQConnectionID(bytes: dcid),
-    Token(bytes: token)
-  )
-}
